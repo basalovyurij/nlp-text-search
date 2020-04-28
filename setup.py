d2d573000a8dfe157e993174ba13d8377d784048
Basalov Yurij <basalov_yurij@mail.ru>
2020-04-28 13:06:17 +0300

    0.3 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nlp-text-search",
-    version="0.2",
+    version="0.3",
     author="Yurij Basalov",
     author_email="basalov_yurij@mail.ru",
     description="A small example package",
