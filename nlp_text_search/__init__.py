3e72c16e55bc14a736a2d0795a2b92d1188bdae7
Basalov Yurij <basalov_yurij@mail.ru>
2020-08-18 13:05:38 +0300

    v0.6.11 fix import

--- nlp_text_search/__init__.py
+++ nlp_text_search/__init__.py
@@ -1,4 +1,4 @@
 from .dists import Dist, LinearizedDist
 from .settings import create_settings
 from .search_engine import AbstractSearchEngine, VPTreeSearchEngine, BruteForceSearchEngine
-from .search_engine import SaveableVPTreeSearchEngine, DefaultSearchEngine
+from .search_engine import SaveableVPTreeSearchEngine, BaseSearchEngine, DefaultSearchEngine
